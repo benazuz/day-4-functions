5e3ebbdc5d26e2604784ce58d9947637e683151f
Benazuz <azuz419@gmail.com>
2019-03-13 17:07:37 +0200

    starting exercise 6.1

--- main.js
+++ main.js
@@ -85,87 +85,253 @@
 // const age = calcAge(2017, 1989)
 
 //exercise 3
-const isEven = function(number){
-    let modul = number % 2
-    if(modul == 0){
-        console.log("the number is even")
-    }else{
-        console.log("the number is uneven")
-    }
-}
+// const isEven = function(number){
+//     let modul = number % 2
+//     if(modul == 0){
+//         console.log("the number is even")
+//     }else{
+//         console.log("the number is uneven")
+//     }
+// }
 
-isEven(99)
-let numbers = [0, 1, 2, 3, 4, 5]
-const areNumbersEven = function(numbers){
-    for(number in numbers){
-        isEven(number)
-    }
-}
+// isEven(99)
+// let numbers = [0, 1, 2, 3, 4, 5]
+// const areNumbersEven = function(numbers){
+//     for(number in numbers){
+//         isEven(number)
+//     }
+// }
 
-areNumbersEven(numbers)
+// areNumbersEven(numbers)
 
-const isThere = function(numbers, number){
-    for(let i of numbers){
-        if(i === number){
-            console.log("The number is there")
-            return true
-        }
-    }
-    console.log("the number does not exist")
-    return false
-}
+// const isThere = function(numbers, number){
+//     for(let i of numbers){
+//         if(i === number){
+//             console.log("The number is there")
+//             return true
+//         }
+//     }
+//     console.log("the number does not exist")
+//     return false
+// }
 
-isThere(numbers, 7)
+// isThere(numbers, 7)
 
-const add = function(a, b){
-    return a + b
-}
+// const add = function(a, b){
+//     return a + b
+// }
 
-const subtract = function(a, b){
-    return a - b
-}
+// const subtract = function(a, b){
+//     return a - b
+// }
 
-const calculator = {add, subtract}
-const result1 = calculator.add(20, 1)
-const result2 = calculator.subtract(30, 9)
-console.log(calculator.add(result1, result2))
+// const calculator = {add, subtract}
+// const result1 = calculator.add(20, 1)
+// const result2 = calculator.subtract(30, 9)
+// console.log(calculator.add(result1, result2))
 
-const turnToKing = function(name, money){
-    name = name.toUpperCase()
-    money = increaseByNameLength(money, name)
-    name = makeRegal(name)
+// const turnToKing = function(name, money){
+//     name = name.toUpperCase()
+//     money = increaseByNameLength(money, name)
+//     name = makeRegal(name)
 
-    console.log(name + " has " + money + " gold coins.")
-}
+//     console.log(name + " has " + money + " gold coins.")
+// }
 
-const increaseByNameLength = function(money, name){
-    let length = name.length
-    money *= length
-    return money
-}
+// const increaseByNameLength = function(money, name){
+//     let length = name.length
+//     money *= length
+//     return money
+// }
 
-const makeRegal = function(name){
-    name = "His Royal Highness " + name
-    return name
-}
+// const makeRegal = function(name){
+//     name = "His Royal Highness " + name
+//     return name
+// }
 
-turnToKing("martin luther", 100)
+// turnToKing("martin luther", 100)
 
-//Scopes
-const isEnough = false
+// //Scopes
+// const isEnough = false
 
-const makeEnough = function () {
-    for (let i = 0; i < 10; i++) {
-        if (i > 5) {
-            isEnough = true
-        }
-    }
-}
+// const makeEnough = function () {
+//     for (let i = 0; i < 10; i++) {
+//         if (i > 5) {
+//             isEnough = true
+//         }
+//     }
+// }
 
-makeEnough()
-if (isEnough) {
-    console.log("Finally, sheesh")
-}
-else {
-    console.log("Here we go again...")
-}
+// makeEnough()
+// if (isEnough) {
+//     console.log("Finally, sheesh")
+// }
+// else {
+//     console.log("Here we go again...")
+// }
+
+//js This
+//exercise 0
+// const counter = {
+//     count: 0,
+
+//     updateCounter: function () {
+//       this.count += 1;
+//     }
+//   };
+
+//   counter.updateCounter();
+//   counter.updateCounter();
+//   counter.updateCounter();
+
+//   alert(counter.count);
+//exercise 1
+//   const person = {
+//     hungry: true,
+
+//     feed: function () {
+//       if (this.hungry) {
+//         hungry = false;
+//         alert('Im no longer hungry!')
+//       }
+//     }
+//   }
+
+// //   person.feed() //should alert "I'm no longer hungry"
+
+// //exercise 2
+
+//   const pump = function (amount) {
+//     this.liters += amount
+//     console.log('You put ' + amount + ' liters in ' + this.name);
+
+//   };
+
+//   const garage = {
+//     car1: {
+//       name: 'Audi',
+//       liters: 3,
+//       fillTank: pump
+//     },
+//     car2: {
+//       name: 'Mercedes',
+//       liters: 1,
+//       fillTank: pump
+//     }
+//   };
+
+//   garage.car1.fillTank(2);
+//   console.log('Audi should have 5 liters: ',  garage.car1.liters);
+
+//   garage.car2.fillTank(30);
+//   console.log('Mercedes should have 31 liters: ', garage.car2.liters);
+
+//   //exercise 4
+//   const tipJar = {
+//     coinCount: 20,
+//     tip: function () {
+//       this.coinCount += 1;
+//     },
+//     stealCoins: function(number){
+//         return this.coinCount -= number
+//     }
+//   };
+
+//   tipJar.tip();
+//   console.log('Tip jar should have 21 coins: ' + tipJar.coinCount);
+
+//   tipJar.stealCoins(3);
+//   console.log('Tip jar should have 18 coins: ' + tipJar.coinCount);
+
+//   tipJar.stealCoins(10);
+//   console.log('Tip jar should have 8 coins: ' + tipJar.coinCount);
+
+//   //exersice 5
+//   const revealSecret = function () {
+//     return this.secret;
+//   };
+
+//   const shoutIt = function (person, func) {
+//     person.revealItAll = func;
+//     const result = person.revealItAll();
+//     alert(person.name + " said: " + result);
+//   };
+
+//   const avi = {
+//     name: "Avi",
+//     secret: "Im scared of snakes!",
+//   };
+
+//   const narkis = {
+//     name: "Narkis",
+//     secret: "I dont have secrets because I'm zen like that."
+//   };
+
+//   shoutIt(avi, revealSecret);
+//   shoutIt(narkis, revealSecret);
+
+//exercise 6
+
+// const makeObjectArray = function (object) {
+//     return Object.keys(object)
+// }
+// const coffeeShop = {
+//     beans: 40,
+
+//     drinkRequirements: {
+//         latte: 10,
+//         americano: 5,
+//         doubleShot: 15,
+//         frenchPress: 12,
+//     },
+
+//     makeDrink: function (drinkType) {
+//         //   let drinks = Object.keys(this.drinkRequirements);
+//         if (this.drinkRequirements[drinkType]) {
+//             if (this.beans > 0) {
+//                 if (this.beans >= this.drinkRequirements[drinkType]) {
+//                     this.beans -= this.drinkRequirements[drinkType]
+//                     console.log("Enjoy your " + drinkType + "!")
+//                     console.log("There are only " + this.beans + " beans left.")
+//                 }else{
+//                     alert("ALL OUT OF BEANS!")
+//                 }
+//             }else{
+//                 console.log("Im sorry we dont have enough beans to make " + drinkType + " at the moment.")
+//             }
+//         }else{
+//             console.log("Im sorry we dont make " + drinkType + " coffee right now")
+//         }
+//     }
+// }
+
+// coffeeShop.makeDrink("latte");
+// coffeeShop.makeDrink("americano");
+// coffeeShop.makeDrink("filtered"); //should alert/console "Sorry, we don't make filtered"
+// coffeeShop.makeDrink("doubleShot");
+// coffeeShop.makeDrink("frenchPress"); //should alert/console "Sorry, we're all out of beans"
+
+//exercise 3
+const pumpFuel = function (plane) {
+    plane.fuel += 1;
+  };
+  
+  const airplane = {
+    fly: function () {
+      if (this.fuel < 2) {
+        return 'on the ground!';
+      }
+      else {
+        return 'flying!';
+      }
+    },
+    fuel: 0
+  };
+  
+  console.log('The plane should not be able to fly (yet): ' + airplane.fly());
+  
+  pumpFuel(airplane);
+  console.log('The plane should STILL not be able to fly: ' + airplane.fly());
+  
+  pumpFuel(airplane);
+  console.log('Take off! ' + airplane.fly());
